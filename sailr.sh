71e95ecc590d2970a190a43a9809aea9fac346c2
Enda Phelan <ephelan@redhat.com>
2019-06-10 19:49:21 +0100

    docs(inline): added comments to script

--- sailr.sh
+++ sailr.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# set values from config file to variables
 function set_config_values() {
   config=sailr.json
 
@@ -15,6 +16,7 @@ function set_config_values() {
   max_length=$(jq -r .length.max $config)
 }
 
+# build the regex pattern based on the config file
 function build_regex() {
   set_config_values
 
@@ -34,16 +36,23 @@ function build_regex() {
   regexp="${regexp}.{$min_length,$max_length}$"
 }
 
-# get the first line of the commit message
-msg=$(head -1 $1)
-
-build_regex
-
-if [[ ! $msg =~ $regexp ]]; then
+# Print out a standard error message which explains
+# how the commit message should be structured
+function print_error() {
   echo -e "\n\e[1m\e[31m[INVALID COMMIT MESSAGE]"
   echo -e "------------------------\033[0m\e[0m"
   echo -e "\e[1mValid types:\e[0m \e[34m${types[@]}\033[0m"
   echo -e "\e[1mMax length (first line):\e[0m \e[34m$max_length\033[0m"
   echo -e "\e[1mMin length (first line):\e[0m \e[34m$min_length\033[0m\n"
+}
+
+# get the first line of the commit message
+ msg=$(head -1 $1)
+
+ build_regex
+
+if [[ ! $msg =~ $regexp ]]; then
+  # commit message is invalid according to config - block commit
+  print_error
   exit 1
 fi
