1d9e961b1f656ca48978dd8f6a8481df04727823
Enda Phelan <ephelan@redhat.com>
2019-12-18 08:36:04 +0000

    refactor: remove global install option

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,24 +5,16 @@ release_tag=master
 script_file="https://raw.githubusercontent.com/craicoverflow/sailr/$release_tag/sailr.sh"
 
 function init {
-    global=false
-    if [ -z "$1" ] || [ $1 = "." ] ; then
-        destination="$PWD/.git/hooks"
-    elif [ $1 = "--global" ] || [ $1 = "-g" ]; then
-        destination="$HOME/.git-templates/hooks"
-        global=true
-    elif [ ! -z $1 ]; then
+    if [ ! -z $1 ]; then
         echo "Unsupported argument '$1'"
     fi
+
+    $destination="$PWD/.git/hooks"
 
     curl $script_file -o "$destination/commit-msg"
     chmod u+x "$destination/commit-msg"
 
-    if [ $global = false ]; then
-        echo -e "\nInstalled Sailr \e[33mcommit-msg\033[0m hook to \e[32m$PWD\033[0m."
-    else
-        echo -e "\nInstalled Sailr as global \e[33mcommit-msg\033[0m Git hook."
-    fi
+    echo -e "\nInstalled Sailr \e[33mcommit-msg\033[0m hook to \e[32m$PWD\033[0m."
     echo "For usage see https://github.com/craicoverflow/sailr#usage"
 }
 
